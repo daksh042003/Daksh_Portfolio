896a145e8ffb4fbc9339d585d3bda274bef500bf
Daksh Pandey (MAQ LLC) <v-dakpandey@microsoft.com>
2025-07-05 21:39:34 +0530

    first commit

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I am passionate about programming and continuously strive to expand my knowledge and skill set.
+              <br />
+              <br />
+              I have proficiency in core programming languages such as{" "}
+              <i>
+                <b className="purple">C++, JavaScript, and Python.</b>
+              </i>
+              <br />
+              <br />
+              My key areas of interest include developing innovative{" "}
+              <i>
+                <b className="purple">web technologies and software products</b>
+              </i>
+              , as well as exploring advancements in{" "}
+              <b className="purple">Data Engineering</b>.
+              <br />
+              <br />
+              I enjoy building robust and scalable applications using{" "}
+              <b className="purple">Node.js</b> and modern JavaScript frameworks such as{" "}
+              <i>
+                <b className="purple">React.js</b>.
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/daksh042003"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/dakshpandey127"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/daksh-pandey-171324253/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/daksh.885"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
